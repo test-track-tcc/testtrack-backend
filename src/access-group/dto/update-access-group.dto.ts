1434316bb88c4e461ca2bd3be52e193bab780940
diogobonet <diogosobezak@gmail.com>
2025-10-21 19:11:52 -0300

    feat: adiciona bugs com rota de edicao de status
    Signed-off-by: diogobonet <diogosobezak@gmail.com>

--- src/access-group/dto/update-access-group.dto.ts
+++ src/access-group/dto/update-access-group.dto.ts
@@ -3,10 +3,6 @@ import { CreateAccessGroupDto } from './create-access-group.dto';
 import { IsArray, IsString, IsUUID } from 'class-validator';
 
 export class UpdateAccessGroupDto extends PartialType(CreateAccessGroupDto) {
-    // Se isso herda todas as propriedades de createaccessgroupdto, então o user id também pode ser passado?
-
-    // UpdateAccessGroupDto herda de CreateAccessGroupDto por utilizar PartialType transforma todos os campos em opcionais.
-    // Id é passado como parâmetro na rota para identificar o grupo de acesso a ser atualizado.
     @ApiProperty()
     @IsArray()
     @IsUUID("all", { each: true })
