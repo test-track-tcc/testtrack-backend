b3d501e5eff896e6f3db2448879b69239c4f30d4
diogobonet <diogosobezak@gmail.com>
2025-11-09 17:02:34 -0300

    fix: ajusta script upload to vercel
    Signed-off-by: diogobonet <diogosobezak@gmail.com>

--- src/utils/script.upload.ts
+++ src/utils/script.upload.ts
@@ -1,12 +1,15 @@
 import { put } from '@vercel/blob';
-import * as fs from 'fs';
 
 export async function uploadScriptToBlob(file: Express.Multer.File) {
-  const fileBuffer = fs.readFileSync(file.path);
-  const blob = await put(`scripts/${file.originalname}`, fileBuffer, {
+  if (!file?.buffer) {
+    throw new Error('Arquivo inválido ou buffer ausente');
+  }
+
+  const blob = await put(`scripts/${file.originalname}`, file.buffer, {
     access: 'public',
     token: process.env.TESTTRACK_READ_WRITE_TOKEN,
-    allowOverwrite: true, 
+    allowOverwrite: true,
   });
+
   return blob.url;
 }
